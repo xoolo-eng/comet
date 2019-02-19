4408dcc285632653871c8c8de52a214e9aa71cf0
Dmitriy Amelchenko <amelchenko.dmitriy@outlook.com>
2019-02-19 21:21:33 +0300

    Select to test socket

--- tests/src/test_socket.cpp
+++ tests/src/test_socket.cpp
@@ -1,11 +1,14 @@
-#include <comet/socket.h>
+#include <comet/socket>
 #include <set>
 #include <algorithm>
+#include <sys/select>
+#include <unistd>
 
 using namespace std;
 
 int main()
 {
+	short result;
 	ServerSocket socket;
 	socket = ServerSocket(AF_INET, SOCK_STREAM);
 	socket.bind("127.0.0.1", 8080);
@@ -13,6 +16,15 @@ int main()
 	socket.nonblock();
 	set<ServerSocket> clients;
 	clients.clear();
-	
+	fd_set readset, writeset, exset;
+	while (true)
+	{
+		FD_ZERO(&readset);
+		FD_ZERO(&writeset);
+		FD_ZERO(&exset);
+		unsigned int maxfd = socket;
+		FD_SET(socket, &readset);
+
+	}
 	return 0;
 }
